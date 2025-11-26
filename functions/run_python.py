b662cd18bddbd38b6e37e0e6da6bc6d823f42bdd
B00m3r0302 <hudson.t.woomer@gmail.com>
2025-11-25 22:49:00 -0500

    completed ch 2

--- functions/run_python.py
+++ functions/run_python.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 
 def run_python_file(working_directory, file_path, args=[]):
@@ -11,4 +12,18 @@ def run_python_file(working_directory, file_path, args=[]):
         return f'Error: File "{file_path}" not found.'
 
     if file_path[-3:] != ".py":
-        return f'Error: "{file_path}" is not a python file.'
+        return f'Error: "{file_path}" is not a Python file.'
+
+    commands = ["python", abs_target, *args]
+    result = subprocess.run(
+        commands, capture_output=True, cwd=abs_working_dir, timeout=30
+    )
+    try:
+        output = f"STDOUT: {result.stdout.decode('utf-8')} | STDERR: {result.stderr.decode('utf-8')}"
+        if result.stdout.decode("utf-8") == "" and result.stderr.decode("utf-8") == "":
+            output = "No output produced"
+        if result.returncode != 0:
+            output += f" Process exited with code {result.returncode}"
+        return output
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
